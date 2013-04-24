714b28e725f91a1f1d64008447afb1c65faaff79
cloveryu <clover210yu@gmail.com>
2013-04-24 23:14:04 +0800

    added default, any Name

--- tailor-core/src/test/java/com/github/tailor/inject/NameTest.java
+++ tailor-core/src/test/java/com/github/tailor/inject/NameTest.java
@@ -1,8 +1,6 @@
 package com.github.tailor.inject;
 
-import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Matchers;
 
 import static com.github.tailor.inject.Name.named;
 import static org.junit.Assert.assertFalse;
@@ -17,12 +15,47 @@ public class NameTest {
 
     @Test
     public void shouldEqualToTheExactSameName() {
-        assertTrue( named("foo").equalTo(named("foo")) );
+        assertTrue(named("foo").equalTo(named("foo")));
     }
 
     @Test
     public void shouldNotEqualToTheDifferentName() {
-        assertFalse( named("foo").equalTo(named("bar")) );
+        assertFalse(named("foo").equalTo(named("bar")));
+    }
+
+    @Test
+    public void shouldBeApplicableToAnyNameGivenWildcard() {
+        assertTrue(named("foo").isApplicableFor(Name.ANY));
+    }
+
+    @Test
+    public void shouldBeApplicableGivenExactSameNameFollowedByWildcard() {
+        assertTrue(named("foo").isApplicableFor(named("foo*")));
+    }
+
+    @Test
+    public void shouldBeApplicableGivenFirstLetterFollowedByWildcard() {
+        assertTrue(named("foo").isApplicableFor(named("f*")));
+    }
+
+    @Test
+    public void shouldBeApplicableGivenStartOfNameFollowedByWildcard() {
+        assertTrue(named("foo").isApplicableFor(named("fo*")));
+    }
+
+    @Test
+    public void shouldBeApplicableToAnyNameGivenDefaultName() {
+        assertTrue(Name.DEFAULT.isApplicableFor(Name.ANY));
+    }
+
+    @Test
+    public void shouldBeApplicableToDefaultNameGivenAnyName() {
+        assertTrue(Name.ANY.isApplicableFor(Name.DEFAULT));
+    }
+
+    @Test
+    public void shouldBeApplicableToWhateverNameGivenAnyName() {
+        assertTrue(Name.ANY.isApplicableFor(named("foo")));
     }
 
 }
