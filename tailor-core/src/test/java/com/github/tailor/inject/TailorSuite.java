80135599431da64ea8ad190292498872b476317f
cloveryu <clover210yu@gmail.com>
2013-04-25 00:13:09 +0800

    added test suite

---
+++ tailor-core/src/test/java/com/github/tailor/inject/TailorSuite.java
@@ -0,0 +1,16 @@
+package com.github.tailor.inject;
+
+import com.github.tailor.inject.bind.PrimitiveBindsTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * User: Clover Yu
+ * Date: 4/25/13
+ * Time: 12:07 AM
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {DeclarationTypeTest.class, NameTest.class, PackagesTest.class, TargetTest.class, TypeTest.class,
+        PrimitiveBindsTest.class})
+public class TailorSuite {
+}
