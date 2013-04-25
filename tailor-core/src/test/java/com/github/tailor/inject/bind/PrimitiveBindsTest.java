d087b6b3c7bb771014c60c1229783e1788d70c24
cloveryu <clover210yu@gmail.com>
2013-04-25 13:14:50 +0800

    added exception handle for di resolve

--- tailor-core/src/test/java/com/github/tailor/inject/bind/PrimitiveBindsTest.java
+++ tailor-core/src/test/java/com/github/tailor/inject/bind/PrimitiveBindsTest.java
@@ -1,9 +1,12 @@
 package com.github.tailor.inject.bind;
 
+import com.github.tailor.inject.DIRuntimeException;
 import com.github.tailor.inject.Injector;
+import com.github.tailor.inject.Name;
 import com.github.tailor.inject.bootstrap.Bootstrap;
 import org.junit.Test;
 
+import static com.github.tailor.inject.DIRuntimeException.*;
 import static com.github.tailor.inject.Dependency.dependency;
 import static com.github.tailor.inject.Name.named;
 import static org.hamcrest.CoreMatchers.is;
@@ -21,7 +24,7 @@ public class PrimitiveBindsTest {
         @Override
         protected void declare() {
             bind( int.class ).to( 1 );
-            bind( named( "pi" ), float.class ).to( 3.1415f );
+            bind( named( "pi" ), float.class ).to(3.1415f);
             bind( Circle.class ).toConstructor();
         }
 
@@ -44,8 +47,8 @@ public class PrimitiveBindsTest {
 
     @Test
     public void shouldWorkAsWrapperClassGivenPrimitiveInt() {
-        assertThat( injector.resolve( dependency( Integer.class ) ), is( 1 ) );
-        assertThat( injector.resolve( dependency( int.class ) ), is( 1 ) );
+        assertThat( injector.resolve( dependency(Integer.class) ), is( 1 ) );
+        assertThat( injector.resolve( dependency(int.class) ), is( 1 ) );
     }
 
     @Test
@@ -59,6 +62,16 @@ public class PrimitiveBindsTest {
         Circle circle = injector.resolve( dependency( Circle.class ) );
         assertThat( circle.r, is( 1 ) );
         assertThat( circle.pi, is( 3.1415f ) );
+    }
+
+    @Test ( expected = NoSuchResourceException.class )
+    public void shouldThrowExceptionWhenResolvingAnUnboundDependency() {
+        injector.resolve( dependency( String.class ) );
+    }
+
+    @Test ( expected = NoSuchResourceException.class )
+    public void shouldThrowExceptionWhenResolvingAnUnboundDependencyWithBoundRawType() {
+        injector.resolve( dependency( float.class ).named( Name.DEFAULT ) );
     }
 
 }
